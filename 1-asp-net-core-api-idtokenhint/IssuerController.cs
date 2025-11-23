dadaec2efbae6138575d528e9e075cb195901212
Artemi Krauczanka <62513530+ArtsiomKrauchanka@users.noreply.github.com>
2025-11-23 18:04:52 +0100

    self attested

--- 1-asp-net-core-api-idtokenhint/IssuerController.cs
+++ 1-asp-net-core-api-idtokenhint/IssuerController.cs
@@ -45,9 +45,9 @@ namespace AspNetCoreVerifiableCredentials
         }
 
         private IssuanceRequest SetClaims( IssuanceRequest request ) {
-            request.claims = new Dictionary<string, string>();
-            request.claims.Add( "given_name", "Megan" );
-            request.claims.Add( "family_name", "Bowen" );
+            //request.claims = new Dictionary<string, string>();
+            //request.claims.Add( "given_name", "Megan" );
+            //request.claims.Add( "family_name", "Bowen" );
 
             string photoClaimName = "";
             // get photo claim from manifest
